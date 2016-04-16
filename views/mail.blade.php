1697379a797c7b4f647a89c14f77ce4c20e09e09
Tasos Protopapas <tprotopgr@gmail.com>
2016-04-16 05:41:48 +0300

    mail page

--- views/mail.blade.php
+++ views/mail.blade.php
@@ -11,44 +11,37 @@
             </div>
         @endif
 
-            <a href="{{url('mailbox')}}" class="btn btn-primary pull-left">Back</a>
-        <form action="{{url("/mailbox/delete/{$thread->id}")}}" method="post" class="ml5">
-            <!-- Submit button -->
-            {{csrf_field()}}
-            <div class="form-group">
-                <button type="submit" class="btn btn-danger btn-md">Delete Message</button>
-            </div>
-        </form>
+
         <div class="panel panel-default mt45">
             <div class="panel-heading intro">
-
                 <h3 class="panel-title">{{$thread->title}}</h3>
             </div>
             <div class="panel-body">
-                @foreach($thread->messages as $index => $msg)
-                    @if($index > 0)
-                        <hr>
-                    @endif
-                    <strong>{{$msg->author->name}}</strong> wrote:
-                    <p class="text-muted pull-right">{{$msg->created_at->format('Y-m-d H:i')}}</p>
-                    <div class="msg">
-                        {!! $msg->body !!}
+                <a href="{{url('mailbox')}}" class="btn btn-primary pull-left">Back</a>
+                <form action="{{url("/mailbox/delete/{$thread->id}")}}" method="post" class="ml5">
+                    <!-- Submit button -->
+                    {{csrf_field()}}
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-danger btn-md">Delete Message</button>
                     </div>
-
+                </form>
+                @foreach($thread->messages as $index => $msg)
+                    <div class="well">
+                        <strong>{{$msg->author->name}}</strong> <span class="text-muted">on {{$msg->created_at->format('Y-m-d H:i')}}</span>:
+                        <hr>
+                        <div class="msg">
+                            {!! $msg->body !!}
+                        </div>
+                    </div>
                 @endforeach
             </div>
         </div>
-
         <form action="{{url('/mailbox/message/'.$thread->id.'/reply')}}" method="post">
             {{csrf_field()}}
             <div class="form-group">
-                <label for="body">Reply</label>
-                <textarea name="body" id="body" cols="30" rows="10" class="form-control"></textarea>
+                <textarea name="body" id="body" cols="30" rows="6" class="form-control"></textarea>
             </div>
-            <button class="btn btn-primary">Send</button>
+            <button class="btn btn-primary">Reply</button>
         </form>
-
     </div>
-
-
 @stop
